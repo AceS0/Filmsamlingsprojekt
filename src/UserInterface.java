b39e028e024116c9dc219e73b8855d0fea2099ce
Enes-Lenovo <enfi0001@stud.kea.dk>
2024-10-24 15:51:46 +0200

    rettet små fejl

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -7,22 +7,26 @@ public class UserInterface {
         boolean running = true;
         Scanner sc = new Scanner(System.in);
 
+        System.out.println(
+                "Welcome to your movie collection.\n"
+                +"1. Create a movie.\n"+
+                "2. Exit.\n" +
+                "3. List of the movies\n" +
+                "4. Get a help list.\n" +
+                "5. Search movie");
         while (running) {
-            System.out.println("Welcome to your movie collection.\n\n"+
-                    "1. Create a movie.\n"+
-                    "2. Exit.\n" +
-                    "3. List of the movies\n" +
-                    "4. Get a help list.");
-            System.out.print("Choose an option: ");
+            System.out.print("\nChoose an option: ");
             String userInput = sc.nextLine().toLowerCase();
             switch (userInput) {
                 case "create","1" -> addMovieByUser();
-                case "exit","2" -> {System.out.println("Thank you for your time, hope to see you again."); return;}
+                case "exit","2" -> {System.out.println("Thank you for your time, hope to see you again.");
+                    return;}
                 case "list","l","3" -> getMovieList();
                 case "help", "h","4" -> helpList();
                 case "search", "s","5" -> searchForFilm();
                 default -> System.out.println("Unknown request, please try again.");
             }
+            System.out.println("Type \"help\", \"h\", \"4\"");
         }
     }
 
@@ -34,8 +38,16 @@ public class UserInterface {
         System.out.print("Who is the director: ");
         String director = sc.nextLine();
         System.out.print("Which year was the movie released: ");
+        while (!sc.hasNextInt()){
+            System.out.print("Invalid input. Please enter a valid number: ");
+            sc.next();
+        }
         int yearCreated = sc.nextInt();
-        System.out.println("Is the movie in color? (Yes/No)");
+
+
+
+
+        System.out.print("Is the movie in color? (Yes/No): ");
         String isInColor = sc.next().toLowerCase();
         while(!isInColor.equals("yes") && !isInColor.equals("no")) {
                 System.out.println("Unknown request, please try again. \"Hint (Yes / No)\"");
@@ -45,7 +57,13 @@ public class UserInterface {
                 }
         }
         System.out.print("How long is the movie (in minutes): ");
+        while (!sc.hasNextInt()) {
+            System.out.print("Invalid input. Please enter a valid number: ");
+            sc.next();
+        }
         int lengthInMinutes = sc.nextInt();
+
+
         System.out.print("In what genre type is the movie: ");
         String genre = sc.next();
         controller.addMovieToCollection(name,director,yearCreated,isInColor,lengthInMinutes,genre);
@@ -61,7 +79,7 @@ public class UserInterface {
                 "Type [1, create] -> Create a movie.\n"+
                 "Type [2, exit] -> Exit the application.\n" +
                 "Type [3, list, l] -> List the movies.\n" +
-                "Type [4, help, h] -> Get a help list.");
+                "Type [4, help, h] -> Get a help list.\n");
     }
 
     public String getMovieDesc(Movie movieName) {
