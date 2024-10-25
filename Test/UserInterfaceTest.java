5cbe56e60a7199513b0c3192fa3379df9d258c3a
XanderKjoller <xander@kjollers.dk>
2024-10-25 11:05:05 +0200

    lavet test

---
+++ Test/UserInterfaceTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserInterfaceTest {
+    Controller cont = new Controller();
+
+    @Test   
+    void addMovieByUser() {
+    }
+
+    @Test
+    void getMovieList() {
+    }
+
+    @Test
+    void helpList() {
+    }
+
+    @Test
+    void getMovieDesc() {
+    }
+
+    @Test
+    void searchForFilm() {
+    }
+
+    @Test
+    void editFilm() {
+    }
+}
